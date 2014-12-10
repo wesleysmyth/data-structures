d4869f902f0004e0a8d952cf9930cb6c57f0069f
wesleysmyth <wesleytatesmith@gmail.com>
2014-12-10 15:30:44 -0800

    Completes functional portion of sprint

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,28 @@
 var Queue = function(){
   var someInstance = {};
-
+  var enqueueCounter = 0;
+  var dequeueCounter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[enqueueCounter] = value;
+    enqueueCounter++;
   };
 
   someInstance.dequeue = function(){
+    if (enqueueCounter-dequeueCounter > 0) {
+      var result = storage[dequeueCounter];
+      delete storage[dequeueCounter];
+      dequeueCounter++;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return (enqueueCounter-dequeueCounter);
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function(){
   var someInstance = {};
-
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.pop = function(){
+    if (counter > 0) {
+      var item = storage[counter];
+      delete storage[counter];
+      counter--;
+      return item;
+    }
   };
 
   someInstance.size = function(){
+    return counter;
   };
 
   return someInstance;
