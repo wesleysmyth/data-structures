8c1851ffb984d47014b8ba5ebbafbfbcd0a7be55
wesleysmyth <wesleytatesmith@gmail.com>
2014-12-10 21:21:26 -0800

    Completes through prototypical inheritance pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  //obj.__proto__ = queueMethods;  -- interesting!!
+  obj.storage = {};
+  obj.enqueueCounter = 0;
+  obj.dequeueCounter = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.enqueueCounter] = value;
+    this.enqueueCounter++;
+  },
 
+  dequeue: function(){
+    if (this.enqueueCounter-this.dequeueCounter > 0) {
+      var result = this.storage[this.dequeueCounter];
+      delete this.storage[this.dequeueCounter];
+      this.dequeueCounter++;
+      return result;
+    }
+  },
+
+  size: function(){
+    return (this.enqueueCounter-this.dequeueCounter);
+  }
+};
+
+//console.log(queueMethods.__proto__);
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.counter = 0;
+  obj.storage = {};
+
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value){
+    this.counter++;
+    this.storage[this.counter] = value;
+  },
+  pop: function(){
+    if (this.counter > 0) {
+      var item = this.storage[this.counter];
+      delete this.storage[this.counter];
+      this.counter--;
+      return item;
+    }
+  },
+  size: function(){
+    return this.counter;
+  }
+};
