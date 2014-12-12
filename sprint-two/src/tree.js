45d194b9a54551c3adbce0b570a7818dbb5e6cfc
wesleysmyth <wesleytatesmith@gmail.com>
2014-12-11 23:04:36 -0800

    Finally finishes tree!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,37 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target){
-
+  // check if 'this' value is === target
+  if (this.value === target) {
+    return true;
+  // else recurse through each child of 'this' node
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      // for each child, capture result of recursive call to var result
+      var result = this.children[i].contains(target);
+      //if result === true (one of child's descendents contains target)
+      if (result) {
+        // bubble up found true
+        return true;
+      }
+    }
+  }
+  // if, no descendents contain target, bubble up "not found"
+  return false;
 };
 
 
